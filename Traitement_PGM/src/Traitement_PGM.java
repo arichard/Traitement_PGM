21a6a40d367664a6b5a8eba381abfe3c6f029562
Antoine Richard <ant.richard@gmail.com>
2011-09-28 11:34:45 +0200

    Lecture d'un fichier PGM pour histogramme

--- Traitement_PGM/src/Traitement_PGM.java
+++ Traitement_PGM/src/Traitement_PGM.java
@@ -1,36 +1,57 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.util.HashMap;
 
 public class Traitement_PGM {
 
-	// Lecture du fichier
-			String filePath = "" ;
+	protected Traitement_PGM() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	// Création d'un histogramme à partir d'un fichier PGM
+	public HashMap<Integer, Integer> lecturePourHistogramme(String cheminFichier) {
+
+		String filePath = cheminFichier;
+
+		HashMap<Integer, Integer> histogramme = new HashMap<Integer, Integer>();
+
+		try {
+			// Création du flux bufférisé sur un FileReader, immédiatement suivi
+			// par un
+			// try/finally, ce qui permet de ne fermer le flux QUE s'il le
+			// reader
+			// est correctement instancié (évite les NullPointerException)
+			BufferedReader buff = new BufferedReader(new FileReader(filePath));
 
 			try {
-				// Création du flux bufférisé sur un FileReader, immédiatement suivi
-				// par un
-				// try/finally, ce qui permet de ne fermer le flux QUE s'il le
-				// reader
-				// est correctement instancié (évite les NullPointerException)
-				BufferedReader buff = new BufferedReader(new FileReader(filePath));
+				String line;
+				// Lecture du fichier ligne par ligne. Cette boucle se termine
+				// quand la méthode retourne la valeur null.
 
-				try {
-					String line;
-					// Lecture du fichier ligne par ligne. Cette boucle se termine
-					// quand la méthode retourne la valeur null.
-					while ((line = buff.readLine()) != null) {
-						System.out.println(line);
-						// faites ici votre traitement
-					}
-				} finally {
-					// dans tous les cas, on ferme nos flux
-					buff.close();
+				// lecture des 4 premières lignes pour les "éviter'
+				for (int i = 1; i <= 4; i++) {
+					line = buff.readLine();
 				}
-			} catch (IOException ioe) {
-				// erreur de fermeture des flux
-				System.out.println("Erreur --" + ioe.toString());
+
+				// traitement des pixels
+				while ((line = buff.readLine()) != null) {
+					histogramme.put(Integer.parseInt(line),
+							histogramme.get(Integer.parseInt(line) + 1));
+				}
+
+			} finally {
+				// dans tous les cas, on ferme nos flux
+				buff.close();
 			}
+		} catch (IOException ioe) {
+			// erreur de fermeture des flux
+			System.out.println("Erreur --" + ioe.toString());
+		}
+
+		return histogramme;
+
+	}
 	
 }
