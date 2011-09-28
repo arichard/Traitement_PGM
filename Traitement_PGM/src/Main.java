516a6f87ce70e2f521f1ff7b6e7ea0a9606f08f6
Antoine Richard <ant.richard@gmail.com>
2011-09-28 09:51:04 +0200

    Creation de classe Main

---
+++ Traitement_PGM/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Antoine
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
